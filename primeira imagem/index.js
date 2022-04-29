25d2377006154628234655a3d2e9d24e76c85921
davidneves11 <davidneves@id.uff.br>
2022-04-28 22:23:08 -0300

    comentários

--- primeira imagem/index.js
+++ primeira imagem/index.js
@@ -7,6 +7,7 @@ app.get("/", (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
+//leitura da variável de ambiente chamada PORT
 app.listen(process.env.PORT, ()=>{
-    console.log("Server is listening on port 3000")
+    console.log(`Server is listening on port ${PORT}`)
 })
